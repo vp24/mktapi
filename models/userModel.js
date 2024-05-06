99642b1f33c67b1eaf0afb2e92911c04624dec0d
“vp24 <vishay8@gmail.com>
2024-05-06 12:23:32 -0500

    less salt

--- models/userModel.js
+++ models/userModel.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', async function (next) {
   try {
-    const salt = await bcrypt.genSalt(10);
+    const salt = await bcrypt.genSalt(5); // Change 10 to 5
     this.password = await bcrypt.hash(this.password, salt);
     next();
   } catch (error) {
