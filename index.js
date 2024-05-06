47d7d32fcb68decd52e9f8563304dc8ccf9c1eab
“vp24 <vishay8@gmail.com>
2024-05-06 12:50:43 -0500

    reverts

--- index.js
+++ index.js
@@ -104,20 +104,17 @@ app.post('/login', async (req, res) => {
   try {
     const { username, password } = req.body;
     console.log('Received login request for username:', username);
-
+    console.log('Password provided:', password); // Add this line
     const user = await User.findOne({ username });
     if (!user) {
       console.log('User not found:', username);
       return res.status(401).json({ error: 'Invalid username or password' });
     }
 
-    // Hash the provided password
-    const hashedPassword = await bcrypt.hash(password, 10); // Use the appropriate salt rounds value
+    console.log('Hashed password from the database:', user.password); // Add this line
 
-    // Compare the hashed passwords
-    const isPasswordValid = await bcrypt.compare(user.password, hashedPassword);
-    console.log('Password comparison result:', isPasswordValid);
-
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    console.log('Password comparison result:', isPasswordValid); // Add this line
     if (!isPasswordValid) {
       console.log('Invalid password for user:', username);
       return res.status(401).json({ error: 'Invalid username or password' });
